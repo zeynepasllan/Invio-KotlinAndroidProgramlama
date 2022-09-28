f137c7c656304a8c1c1149d96fba991d3964705b
zeynepasllan <90160240+zeynepasllan@users.noreply.github.com>
2022-09-28 04:03:18 +0300

    Odev1

---
+++ Odev1/app/src/main/java/com/example/odev1/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.odev1
+
+fun main(){
+    val islemNo:Int = 111
+    val ogrNo:Int = 182113
+    var kitapNo:Int = 1234
+    val aTarih:String = "28.09.2022"
+    val vTarih:String = "05.10.2022"
+
+    val ogrAd:String = "Zeynep"
+    val ogrSoyad:String = "Aslan"
+    var cinsiyet:Char = 'K'
+    val dTarih:String = "30.08.2000"
+    val sinif:Int = 4
+
+    val isbNo:Int = 123
+    var kitapAdi:String = "Olasılıksız"
+    var yazarNo:Int = 18
+    var turNo:Int = 3
+    var sayfaSayisi:Int = 300
+    var puan:Double = 9.5
+
+    var yazarAd = "Adam"
+    var yazarSoyad = "Fawer"
+
+    var turAdi = "Bilimkurgu"
+}
